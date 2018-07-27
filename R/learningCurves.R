d0eb4f71df1fbf9bac35a082284081698491fb81
Raphael Gastrock <raphaelgastrock@gmail.com>
2018-07-27 11:44:52 -0400

    set up some files and directories for the project

---
+++ R/learningCurves.R
@@ -0,0 +1,13 @@
+source('R/shared.R')
+
+plotLearningCurves <- function(target='inline') {
+  
+  # do stuff!
+  
+}
+
+learningCurveANOVA <- function() {
+  
+  # write code!
+  
+}
---
+++ R/shared.R
@@ -0,0 +1,8 @@
+library('ez')
+# library('svglite')
+
+colorPalette <- function() {
+  
+  # return a set of colors that adhere to lab standards
+  
+}
